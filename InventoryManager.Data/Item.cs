6c9a7e747630a4f053dfe8f7b67929ea18a0121d
DWill2121 <37192333+DWill2121@users.noreply.github.com>
2019-11-06 14:59:23 -0500

    Add files via upload

---
+++ InventoryManager.Data/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace InventoryManager.Data
+{
+    public class Item : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Name { get; set; }
+
+
+    }
+}
