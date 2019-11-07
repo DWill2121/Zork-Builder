2ecbb4add3f476e151046a0aae3bde618f1554b1
Tyrell Adams <adams.tyrell@ymail.com>
2019-11-06 22:47:00 -0500

    Restructured folders and uploaded my winform tut

---
+++ Part17Tut/InventoryManager.WinForms/Forms/AddPlayerForm.Designer.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace InventoryManager.WinForms.Forms
+{
+    partial class AddPlayerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.cancleButton = new System.Windows.Forms.Button();
+            this.okButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(15, 25);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(206, 20);
+            this.nameTextBox.TabIndex = 1;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(12, 9);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(35, 13);
+            this.nameLabel.TabIndex = 0;
+            this.nameLabel.Text = "Name";
+            // 
+            // cancleButton
+            // 
+            this.cancleButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancleButton.Location = new System.Drawing.Point(146, 51);
+            this.cancleButton.Name = "cancleButton";
+            this.cancleButton.Size = new System.Drawing.Size(75, 23);
+            this.cancleButton.TabIndex = 3;
+            this.cancleButton.Text = "&Cancle";
+            this.cancleButton.UseVisualStyleBackColor = true;
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(60, 51);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 2;
+            this.okButton.Text = "&Ok";
+            this.okButton.UseVisualStyleBackColor = true;
+            // 
+            // AddPlayerForm
+            // 
+            this.AcceptButton = this.okButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.cancleButton;
+            this.ClientSize = new System.Drawing.Size(233, 92);
+            this.Controls.Add(this.cancleButton);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.nameLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AddPlayerForm";
+            this.ShowInTaskbar = false;
+            this.Text = "AddPlayerForm";
+            AddPlayerForm addPlayerForm = this;
+            
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+       
+
+        #endregion
+
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Button cancleButton;
+        private System.Windows.Forms.Button okButton;
+    }
+}
---
+++ Part17Tut/InventoryManager.WinForms/Forms/AddPlayerForm.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+
+namespace InventoryManager.WinForms.Forms
+{
+    public partial class AddPlayerForm : Form
+    {
+        public string PlayerName 
+        { 
+            get => nameTextBox.Text;
+            set => nameTextBox.Text = value; 
+        }
+        public AddPlayerForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
